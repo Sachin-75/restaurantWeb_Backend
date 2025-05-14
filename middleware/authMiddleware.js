536dde6de7dc0cfa342fd9a62aef0eee989d50f4
Sachin Kumavat <110448909+Sachin-75@users.noreply.github.com>
2025-05-14 21:57:10 +0530

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
     const token = req.headers.authorization?.split(' ')[1]; // Get token from 'Bearer' header
+    console.log("token",token)
     if (!token) {
         return res.status(401).json({ message: 'No token provided' });
     }
@@ -11,8 +12,11 @@ const authMiddleware = (req, res, next) => {
         console.log("Decoded token ",decodedToken);
         req.userId = decodedToken.id; // Extract the user ID from the token and add to request object
 
-        next(); 
+        next(); // Proceed to the next middleware
     } catch (error) {
+        if (error.name === 'TokenExpiredError') {
+            return res.status(401).json({ message: 'Token has expired' });
+        }
         console.error('Token verification error:', error);
         return res.status(401).json({ message: 'Invalid token' });
     }
