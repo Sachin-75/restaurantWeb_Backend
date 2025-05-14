81cb2e2841da5b63fad65f1cedf37e47fd2e8a2d
Sachin Kumavat <110448909+Sachin-75@users.noreply.github.com>
2025-05-14 21:58:02 +0530

    Update cart-router.js

--- router/cart-router.js
+++ router/cart-router.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const {addCartItems, getCartItems} = require("../controllers/cartController");
+const {addCartItems, getCartItems, deleteCartItems, incrementCartItemQuantity, decrementCartItemQuantity} = require("../controllers/cartController");
 const authMiddleware = require("../middleware/authMiddleware");
 
 router.route("/get").get(authMiddleware, getCartItems);
-
-router.route("/add").post( addCartItems);
+router.delete('/delete', authMiddleware, deleteCartItems);
+router.post("/add",authMiddleware, addCartItems);
+router.put("/increment", authMiddleware, incrementCartItemQuantity)
+router.put("/decrement", authMiddleware, decrementCartItemQuantity)
 
 module.exports = router;
