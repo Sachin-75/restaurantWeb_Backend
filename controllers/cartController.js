ed80951b056d026dfe181fd917ec702abfa67822
Sachin-75 <sachin.kumavat93@gmail.com>
2025-05-15 19:45:22 +0530

    Save local changes before pull

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,36 +1,5 @@
 const cartModel = require("../models/cartModel");
 
-// const addCartItems = async (req, res) => {
-//     const { menuItemId, name, quantity, image, price } = req.body;
-//     const userId = req.userId;  // From token, extracted in middleware
-
-//     try {
-//         let cartItem = await cartModel.findOne({ menuItemId });
-
-//         if (cartItem) {
-//             // Update quantity if the item is already in the cart
-//             cartItem.quantity += quantity;
-//             cartItem = await cartItem.save();
-//         } else {
-//             // Add userId manually to new cart item (not changing schema, just attaching for this session)
-//             cartItem = new cartModel({
-//                 menuItemId,
-//                 name,
-//                 quantity,
-//                 image,
-//                 price,
-//                 userId  // Attach userId here temporarily
-//             });
-//             await cartItem.save();
-//         }
-
-//         res.status(200).json(cartItem);
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ error: 'Internal server error' });
-//     }
-// };
-
 const addCartItems = async (req, res) => {
     const { menuItemId, name, quantity, image, price } = req.body;
     console.log("request body==>",menuItemId, name, quantity, image, price)
